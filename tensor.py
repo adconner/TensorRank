ea788a8e1fe8eea7df7997e7a024bda68d41cdef
Austin Conner <aconner.vu@gmail.com>
2025-05-27 17:43:21 -0400

    more experimentation

--- tensor.py
+++ tensor.py
@@ -9,9 +9,20 @@ import equinox as eqx
 from itertools import *
 import time
 
-numit = 100000
-batch = 1000
-printevery = 1000
+
+# import os
+# os.environ["XLA_FLAGS"] = (
+#     "--xla_gpu_enable_triton_softmax_fusion=true "
+#     "--xla_gpu_triton_gemm_any=false "
+#     "--xla_gpu_enable_async_collectives=true "
+#     "--xla_gpu_enable_latency_hiding_scheduler=true "
+#     "--xla_gpu_enable_highest_priority_async_stream=true "
+# )
+
+
+numit = 10000
+batch = 10000
+printevery = 100
 cx = False
 # m,n,l,r = 2,2,2,7
 m,n,l,r = 3,3,3,23
@@ -33,10 +44,12 @@ def residual_function_fp(T):
     def rs(dec):
         r = dec[0].shape[1]
         a,b,c = T.shape
-        A = lx.MatrixLinearOperator(jnp.einsum('jl,kl->jkl', dec[1],dec[2]).reshape(b*c,r))
-        B = lx.MatrixLinearOperator(jnp.einsum('kl,il->kil', dec[2],dec[0]).reshape(c*a,r))
-        C = lx.MatrixLinearOperator(jnp.einsum('il,jl->ijl', dec[0],dec[1]).reshape(a*b,r))
-        solve = jax.vmap(lambda A,B: lx.linear_solve(A, B, lx.AutoLinearSolver(well_posed=None)).value, [None,1], 1)
+        A = jnp.einsum('jl,kl->jkl', dec[1],dec[2]).reshape(b*c,r)
+        B = jnp.einsum('kl,il->kil', dec[2],dec[0]).reshape(c*a,r)
+        C = jnp.einsum('il,jl->ijl', dec[0],dec[1]).reshape(a*b,r)
+        # solve = jax.vmap(lambda A,B: lx.linear_solve(lx.MatrixLinearOperator(A), B, lx.AutoLinearSolver(well_posed=None)).value, [None,1], 1)
+        solve = jax.vmap(lambda A,B: lx.linear_solve(lx.MatrixLinearOperator(A.conj().T @ A, lx.positive_semidefinite_tag), 
+                                                     A.conj().T @ B, lx.AutoLinearSolver(well_posed=True)).value, [None,1], 1)
         f1 = solve(A, T.transpose(1,2,0).reshape(b*c,a)).T
         f2 = solve(B, T.transpose(2,0,1).reshape(c*a,b)).T
         f3 = solve(C, T.transpose(0,1,2).reshape(a*b,c)).T
@@ -48,10 +61,14 @@ def residual_function_elim(T):
         assert len(dec) == 2
         r = dec[0].shape[1]
         a,b,c = T.shape
-        C = lx.MatrixLinearOperator(jnp.einsum('il,jl->ijl', dec[0],dec[1]).reshape(a*b,r))
-        solve = jax.vmap(lambda A,B: lx.linear_solve(A, B, lx.AutoLinearSolver(well_posed=None)).value, [None,1], 1)
+        C = jnp.einsum('il,jl->ijl', dec[0],dec[1]).reshape(a*b,r)
+        solve = jax.vmap(lambda A,B: lx.linear_solve(lx.MatrixLinearOperator(A.conj().T @ A, lx.positive_semidefinite_tag), 
+                                                     A.conj().T @ B, lx.AutoLinearSolver(well_posed=True)).value, [None,1], 1)
+        # solve = jax.vmap(lambda A,B: lx.linear_solve(lx.MatrixLinearOperator(A), B, lx.SVD()).value, [None,1], 1)
+        # solve = jax.vmap(lambda A,B: lx.linear_solve(lx.MatrixLinearOperator(A), B, lx.NormalCG(rtol=1e-2, atol=1e-3)).value, [None,1], 1)
+        # solve = jax.vmap(lambda A,B: lx.linear_solve(lx.MatrixLinearOperator(A), B, lx.AutoLinearSolver(well_posed=None)).value, [None,1], 1)
         f3 = solve(C, T.reshape(a*b,c)).T
-        return (jax.vmap(C.mv, out_axes=1)(f3).reshape(T.shape) - T,),dec+(f3,)
+        return ((C @ f3.T).reshape(T.shape) - T,),dec+(f3,)
     return rs
     
 def basic_loss_function(T):
@@ -70,25 +87,26 @@ def clipped(x):
         return jnp.clip(x,min=-cl_bound,max=cl_bound)
     
 def loss_function(T):
-    # rs = residual_function_elim(T)
-    rs = residual_function(T)
+    rs = residual_function_elim(T)
+    # rs = residual_function(T)
+    # rs = residual_function_fp(T)
     def loss_fn(dec,it,rng):
         progress = it / numit
-        r,dec = rs(dec)
+        r,full_dec = rs(dec)
         loss = jax.tree.reduce(jnp.add,jax.tree.map(lambda e: jnp.mean(optax.l2_loss(jnp.abs(e))), r))
-        for f in dec:
-            loss += 0.003 * 0.1 ** (progress*4) * jnp.mean(jnp.abs(f))
-            loss += 0.01 * 0.1 ** (progress*4) * jnp.mean(optax.l2_loss(jnp.abs(f)))
+        for f in full_dec:
+            loss += 0.003 * jnp.mean(jnp.abs(f))
+            loss += 0.02 * 0.1 ** (progress*4) * jnp.mean(optax.l2_loss(jnp.abs(f)))
             # loss += 0.03 * ((1-jnp.cos(jnp.pi*progress))/2) * jnp.mean(optax.l2_loss(jnp.abs(f - jnp.round(f))))
             # loss += 0.01 * ((1-jnp.cos(jnp.pi*progress))/2)**2 * jnp.mean(optax.l2_loss(jnp.abs(f - jnp.round(f*2)/2)))
             # loss += 0.1 * jnp.mean(optax.l2_loss(jnp.abs(f-clipped(f))))
-        return 1000*loss
+        return 1000*loss,full_dec
     return loss_fn
 
 def init(rng):
     dec = []
-    for td in T.shape:
-    # for td in T.shape[:-1]:
+    # for td in T.shape:
+    for td in T.shape[:-1]:
         rng, rngcur = jax.random.split(rng)
         if cx:
             dec.append(jax.random.normal(rng, (td, r), jnp.complex64))
@@ -102,8 +120,7 @@ T = matrixmult(m,n,l)
 if cx:
     T = T.astype('complex64')
 loss_fn = loss_function(T)
-basic_loss_fn = jax.jit(jax.vmap(basic_loss_function(T)),device=jax.devices('cpu')[0])
-# basic_loss_fn = jax.jit(jax.vmap(basic_loss_function_elim(T)),device=jax.devices('cpu')[0])
+basic_loss_fn = jax.vmap(basic_loss_function(T))
 
 rng = jax.random.PRNGKey(np.random.randint(2**32))
 
@@ -119,7 +136,7 @@ opt_state = jax.vmap(optimizer.init)(dec)
 @functools.partial(jax.jit,donate_argnums=[0,1])
 @functools.partial(jax.vmap, in_axes=[0,0,None,None])
 def update_function(dec, opt_state, it, rng):
-    loss, grads = jax.value_and_grad(loss_fn)(dec,it,rng)
+    (loss, full_dec), grads = jax.value_and_grad(loss_fn,has_aux=True)(dec,it,rng)
     if cx:
         grads = jax.tree.map(jnp.conj, grads)
     # grads = jax.tree.map(jax.add, grads, 
@@ -128,31 +145,38 @@ def update_function(dec, opt_state, it, rng):
     # grads = jax.tree.map(lambda e: e + 0.02*jax.random.normal(rng, e.shape, e.dtype),grads)
     updates, opt_state = optimizer.update(grads, opt_state, dec, value = loss, grad = grads, value_fn = lambda x: loss_fn(x,it,rng))
     dec = optax.apply_updates(dec, updates)
-    maxabs = jax.tree.reduce(jnp.maximum,jax.tree.map(lambda t: jnp.max(jnp.abs(t)),dec))
-    return dec, opt_state, loss, maxabs
+    return dec, opt_state, loss, full_dec
+
+@jax.jit
+def stats(opt_state, loss, full_dec):
+    besti = jnp.argpartition(loss,4)[:5]
+    besti = besti[jnp.argsort(loss[besti])]
+    # besti = jnp.sort(besti)
+    rfd = jax.tree.map(lambda e: e[besti], full_dec)
+    bloss = basic_loss_fn(rfd)
+    maxabs = jax.vmap(lambda dec: jax.tree.reduce(jnp.maximum,jax.tree.map(lambda e: jnp.max(jnp.abs(e)), dec)))(rfd)
+    worst = jnp.max(loss)
+    return besti,loss[besti],bloss,maxabs,worst
 
 # A simple update loop.
 for it in range(numit):
     if it == 1:
         startt = time.time()
     rng, rngcur = jax.random.split(rng)
-    dec, opt_state, loss, maxabs = update_function(dec, opt_state, it, rngcur)
-    # if it % 10 == 9:
+    dec, opt_state, loss, full_dec = update_function(dec, opt_state, it, rngcur)
     if it % printevery == printevery-1:
-        besti = jnp.argpartition(loss,4)[:5]
-        besti = besti[jnp.argsort(loss[besti])]
-        # besti = jnp.sort(besti)
+        besti, closs, bloss, maxabs, worst = stats(opt_state,loss,full_dec)
         elapsed = time.time() - startt
         print(f'{it}: time elapsed {elapsed}, Kips {it*batch / (1000*elapsed) }')
         print(besti)
-        print(loss[besti])
-        print(basic_loss_fn(dec)[besti])
-        print(maxabs[besti])
-        print(jnp.max(loss))
-        # print(loss)
+        print(loss)
+        print(bloss)
+        print(maxabs)
+        print(worst)
         
 # startt = time.time()
-# numrefine = min(500,batch)
+# numrefine=10
+# numrefine = min(numrefine,batch)
 # loss = basic_loss_fn(dec)
 # besti = jnp.argpartition(loss,numrefine-1)[:numrefine]
 # decbest = jax.tree.map(lambda x: x[besti],dec)
@@ -175,8 +199,8 @@ for it in range(numit):
 #         #     # rs.append(0.1 * (f - jnp.round(f)))
 #         #     rs.append(0.5 * (f - clipped(f)))
 #         return rs
-#     return optx.root_find(rs_fn, lm, dec, throw=False).value
-#     # return optx.least_squares(rs_fn, lm, dec, throw=False).value
+#     # return optx.root_find(rs_fn, lm, dec, throw=False).value
+#     return optx.least_squares(rs_fn, lm, dec, throw=False).value
 # dec_refine = refine(decbest)
 # print( basic_loss_fn(dec_refine) )
 # print(f'time elapsed : {time.time() - startt}')
